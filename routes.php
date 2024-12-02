509e2e749d35702cd9facb34a00623b5abd95e6a
Zeerak-Zubair <zeerakzubair047@gmail.com>
2024-12-02 14:57:16 +0500

    Updated routes.php to declare routes in Router class

--- routes.php
+++ routes.php
@@ -1,10 +1,16 @@
 <?php 
 
-return [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
+// return [
+//     '/' => 'controllers/home.php',
+//     '/listings' => 'controllers/listings/index.php',
+//     '/listings/create' => 'controllers/listings/create.php',
+//     //'404' => 'controllers/error/404.php' // removed since we configured this path in the Controller Class
+// ];
+
+//Populating the routes array in the $router object 
+
+$router->get('/','controllers/home.php');
+$router->get('/listings','controllers/listings/index.php');
+$router->get('/listings/create','controllers/listings/create.php');
 
 ?>
