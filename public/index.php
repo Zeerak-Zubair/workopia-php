c93ab36ac395a8178def125430d856f020a9a673
Zeerak-Zubair <zeerakzubair047@gmail.com>
2024-12-02 14:58:34 +0500

    Refactored public/index.php to use Router class

--- public/index.php
+++ public/index.php
@@ -5,11 +5,24 @@
 //php -S localhost -t public
 
 //We pasted the css and images folders in the public directory as well
+
 require '../helper.php';
 
+//Necessary to create the Router object
+require basePath('Router.php');
+
+//Instantiating the Router class
+$router = new Router();
+
+//Initializing the routes configured to specific URIs
+$routes = require basePath('routes.php');
+
+//obtaining the URI
 $uri = $_SERVER['REQUEST_URI'];
+//obtaining the REQUEST METHOD
 $method = $_SERVER['REQUEST_METHOD'];
 
-require basePath('router.php');
+//Routing the requested URI and Method
+$router->route($uri,$method);
 
 ?>
